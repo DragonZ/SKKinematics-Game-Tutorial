a4f587795e12ffaea54f84da27fbed143dc9e2c3
Long Zhen <lzhen@Longs-MBP.attlocal.net>
2016-03-30 13:01:59 -0700

    initial commit

---
+++ IK-Ninja/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  IK-Ninja
+//
+//  Created by Ken Toh on 7/9/14.
+//  Copyright (c) 2014 Ken Toh. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+  
+  override func didMoveToView(view: SKView) {
+   
+  }
+}
